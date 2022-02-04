4b0b93febbdf9640972ec398cfc112c00716bc9b
Hernan <hernan@mukiku.com>
2022-02-04 12:32:27 -0300

    'finish'

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+def palindromo(palabra):
+    p = palabra.replace(' ', '').lower()
+    p_inv = p[::-1]
+    if p == p_inv:
+        return True
+    else:
+        return False
+
+
 def main():
-    pass
+    word = input('Ingresa la palabra a verificar: ')
+    es_palindromo = palindromo(word)
+    if es_palindromo == True:
+        print('La palabra es palindromo')
+    else:
+        print('La palabra no es palindromo')
 
 
 if __name__ == '__main__':
